7b48ac94a9afcdcfeb07747b28d3e2112d4f4306
Chanary Wilson <chanarykw@gmail.com>
2024-08-23 14:49:30 -0700

    LandingPage component

---
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import './LandingPage.css';
+
+const LandingPage = () => {
+    return (
+        <section>
+            <h1>Landing Page</h1>
+        </section>
+    )
+};
+
+export default LandingPage;
