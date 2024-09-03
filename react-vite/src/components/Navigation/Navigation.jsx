d35dc245ee709dc8d6758993129264e55bd8a467
Chanary Wilson <137831268+vinob09@users.noreply.github.com>
2024-09-02 21:37:20 -0700

    Fixed notes frontend (#10)

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,15 +1,24 @@
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
 
 function Navigation({ className }) {
+  const user = useSelector(state => state.session.user);
+
   return (
     <nav className={`navbar ${className}`}>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link" href="/">
-            Home
-          </a>
+          {user ? (
+            <a className="nav-link" href={`/client/${user.id}`}>
+              Dashboard
+            </a>
+          ) : (
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          )}
         </li>
         <li className="nav-item">
           <ProfileButton />
