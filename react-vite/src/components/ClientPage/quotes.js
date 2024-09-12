93c35c8e7ea48a26db8ee95f83ac3d659640fede
Chanary Wilson <chanarykw@gmail.com>
2024-09-12 12:02:43 -0700

    Fixed ClientPage styling

---
+++ react-vite/src/components/ClientPage/quotes.js
@@ -0,0 +1,50 @@
+export const skyrimQuotes = [
+    "I used to be an adventurer like you. Then I took an arrow in the knee... – Guards",
+    "Dragonborn, huh? Was it your ma or your pa that was the dragon? – Guards and Mercenaries",
+    "Nords are so serious about beards. So many beards. M'aiq thinks they wish they had glorious manes like Khajiit. – M'aiq the Liar",
+    "Iron sword huh? What are you killing? Butterflies? – Guards",
+    "Let me guess, someone stole your sweetroll? – Guards",
+    "I fight so that all the fighting I've already done hasn't been for nothing. I fight because I must. – Ulfric Stormcloak",
+    "Is there singing in the Void? Dancing? Surely the Dread Lord will at least allow poor Cicero to caper... – Cicero",
+    "It would take a month to explain to you how that very question doesn't even make sense. – Urag gro-Shub",
+    "What is better - to be born good, or to overcome your evil nature through great effort? – Paarthurnax",
+    "Hail summoner, conjure me up a warm bed would you? – Guards",
+    "Here in his shrine that they have forgotten, here do we toil that we might remember.... – Miraak",
+    "Skyrim's harshness has a way of carving a man down to his true self. – General Tullius",
+    "Power. You have it, as do all Dov. But power is inert without action and choice. – Paarthurnax",
+    "Skyrim is now host to giant, flying lizards and two-legged cat-men, and you're surprised by me? Yes. I just talked. And am continuing to do so. – Barbas"
+];
+
+export const lotrQuotes = [
+    "I wish it need not have happened in my time, said Frodo. So do I, said Gandalf and so do all who live to see such times. But that is not for them to decide. All we have to decide is what to do with the time that is given us. – Frodo & Gandalf",
+    "Courage is found in unlikely places. – Gildor",
+    "It’s a dangerous business, Frodo, going out your door. You step onto the road, and if you don’t keep your feet, there’s no knowing where you might be swept off to. – Bilbo",
+    "May it be a light to you in dark places, when all other lights go out. – Galadriel",
+    "It is not despair, for despair is only for those who see the end beyond all doubt. We do not. – Gandalf",
+    "The Road goes ever on and on... – Frodo",
+    "Even the smallest person can change the course of the future. – Samwise",
+    "You cannot pass. I am a servant of the Secret Fire, wielder of the flame of Anor. You cannot pass. The dark fire will not avail you, flame of Udûn. Go back to the Shadow! You cannot pass. – Gandalf",
+    "Well, here at last, dear friends, on the shores of the Sea comes the end of our fellowship. – Gandalf",
+    "Let them march now and sing! We have a long way to go and there is time ahead for thought. It is something to have started. – Treebeard",
+    "Only a small part is played in great deeds by any hero. – Gandalf",
+    "Yet do not cast all hope away. Tomorrow is unknown. Rede oft is found at the rising of the Sun. – Legolas"
+];
+
+export const warhammerQuotes = [
+    "Only in death does duty end is no way to live. – Roboute Guilliman",
+    "In the Emperor’s name, let none survive! – Imperial Guardsman",
+    "Give me a hundred Space Marines. Or failing that, give me a thousand other troops. – Rogal Dorn",
+    "Courage and Honour. – Ultramarines",
+    "For in the grim dark future, there is only war. – Narrator Warhammer 40k",
+    "A hero is no braver than an ordinary man, but he is brave five minutes longer. – Rogal Dorn",
+    "By His will alone is Terra kept safe. – Adeptus Custodes",
+    "I am the hammer. I am the edge of His Sword, I am the tip of His Spear, I am the mail about His Fist. I am the bane of His foes and the woes of the treacherous. I am the end. – Grey Knights",
+    "Success is commemorated; Failure merely remembered. – Space Marine",
+    "Trust in the God-Emperor. But trust your blade, too. – Inquisitor Atellus Thrax",
+    "Victory needs no explanation, defeat allows none. – Space Marine",
+    "Faith without deeds is worthless. – Sister Miriya",
+    "The most dangerous weapon is the one no one expects. – Inquisitor Gregor Eisenhorn",
+    "Zeal is its own Excuse. – Sisters of Battle Codex"
+];
+
+export const allQuotes = [...skyrimQuotes, ...lotrQuotes, ...warhammerQuotes];
